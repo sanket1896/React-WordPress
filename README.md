d5072695ff84053c6f730bcc2a1543fd1076a51c
sanket1896 <sanket18960@gmail.com>
2018-11-11 23:11:11 +0530

    Fix gh page

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 In this project
   - This is blog frontend build with React JS
   - Backend with Wordpress API to fetch data on clientside
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Posts from './components/Posts';
-import PostPage from './components/PostPage';
-import NormalHeader from './components/NormalHeader';
-import StickyHeader from './components/StickyHeader';
-import './App.css';
+import Posts from "./components/Posts";
+import PostPage from "./components/PostPage";
+import NormalHeader from "./components/NormalHeader";
+import StickyHeader from "./components/StickyHeader";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <div>
           <NormalHeader />
-          <StickyHeader containerTop={50}/>
-          <Route path="/" component={Posts} exact={true}/>
-          <Route path="/post/:id" component={PostPage} />
+          <StickyHeader containerTop={50} />
+          <Switch>
+            <Route path="/" component={Posts} exact={true} />
+            <Route path="/post/:id" component={PostPage} />
+            <Route component={() => <div>404 not found</div>} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
   }
 }
-
 
 export default App;
