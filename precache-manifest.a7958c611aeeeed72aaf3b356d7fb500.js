21981dddb44f7b9129b0a2e37cc8cb90569e42c6
sanket1896 <sanket18960@gmail.com>
2018-11-11 19:44:59 +0530

    Updates

---
+++ precache-manifest.a7958c611aeeeed72aaf3b356d7fb500.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdf7e07737d9861bd1ff",
+    "url": "/reactwp/static/js/runtime~main.fdf7e077.js"
+  },
+  {
+    "revision": "4f52360e86bd19314be4",
+    "url": "/reactwp/static/js/main.4f52360e.chunk.js"
+  },
+  {
+    "revision": "ee5ce94bb3c166200a42",
+    "url": "/reactwp/static/js/1.ee5ce94b.chunk.js"
+  },
+  {
+    "revision": "4f52360e86bd19314be4",
+    "url": "/reactwp/static/css/main.7e1b9b01.chunk.css"
+  },
+  {
+    "revision": "ee5ce94bb3c166200a42",
+    "url": "/reactwp/static/css/1.3edf4068.chunk.css"
+  },
+  {
+    "revision": "bcd9356c5340f8f89484bbcf5001bba6",
+    "url": "/reactwp/index.html"
+  }
+];
